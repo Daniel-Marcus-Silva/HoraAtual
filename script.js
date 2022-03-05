df3f0f1dd640dc5f422edfcd44486bcb9ca0f7c7
Daniel-Marcus-Silva <95720605+Daniel-Marcus-Silva@users.noreply.github.com>
2022-03-04 21:16:20 -0300

    Retirado comentário.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
-    var hora = 13 //data.getHours()
+    var hora = data.getHours()
     var min = data.getMinutes()
     
     msg.innerHTML = `Agora são ${hora} horas e ${min} minutos.`
